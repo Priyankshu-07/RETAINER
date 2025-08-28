1933193c175b65c9a9ddc59b7bf48db984a5f976
Priyankshu-07 <priyankshusinha98@gmail.com>
2025-08-28 19:06:43 +0530

    some changes in frontend and a liitle update in the ml part

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -4,7 +4,7 @@ import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Predict from './pages/Predict';
 import About from './pages/About';
-import Contact from './pages/Contact';
+
 
 function App() {
   return (
@@ -15,7 +15,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/predict" element={<Predict />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
